9e90d5b0a5f158351033582362d9a47e37d9a3c2
nyj001012 <nyj001012@gmail.com>
2022-04-09 22:44:43 +0900

    Initial commit

---
+++ src/main/java/com/nyj001012/balancegame/BalanceGameApplication.java
@@ -0,0 +1,13 @@
+package com.nyj001012.balancegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalanceGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyj001012/balancegame/BalanceGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyj001012.balancegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
